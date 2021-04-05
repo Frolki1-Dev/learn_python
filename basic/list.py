50534434d3e573177a88369eb50f461116ac810f
Frank Giger <18479991+Frolki1-Dev@users.noreply.github.com>
2021-04-05 18:29:11 +0200

    Working with lists

---
+++ basic/list.py
@@ -0,0 +1,21 @@
+# How to work with lists
+
+students = [
+    "Monika",
+    "Fritz",
+    "Luise",
+    "Andi"
+]
+
+print(students)
+
+# Add item to list
+students.append("Ronald")
+
+print(students)
+
+# Get the length of the list
+print(len(students))
+
+# Get a specific student
+print(students[2])
