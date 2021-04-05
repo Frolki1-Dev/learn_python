c8fb44859bab0125c4e85ffd2a6d7a9adb4db1c2
Frank Giger <18479991+Frolki1-Dev@users.noreply.github.com>
2021-04-05 18:23:56 +0200

    Working with strings

---
+++ basic/stings.py
@@ -0,0 +1,11 @@
+# Working with strings
+
+print("Hello world!")
+
+# Combine strings
+language = "Python"
+print("I learning " + language)
+
+# Convert to string
+age = 23
+print("I'm " + str(age) + " old.")
